bcc81813bb52fa9212bc0f6229de0c5a8660b829
sakurakyuo <sakurakyuo@protonmail.com>
2025-09-25 21:20:59 +0800

    corvette: Initial InfinityX
    * OFFICIAL Now

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_corvette.mk
+    $(LOCAL_DIR)/infinity_corvette.mk
--- infinity_corvette.mk
+++ infinity_corvette.mk
@@ -11,10 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from corvette device
 $(call inherit-product, device/oneplus/corvette/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+INFINITY_BUILD_TYPE := OFFICIAL
+INFINITY_MAINTAINER := "SakuraKyuo"
+TARGET_HAS_UDFPS := true
+WITH_GAPPS := true
 
-PRODUCT_NAME := lineage_corvette
+PRODUCT_NAME := infinity_corvette
 PRODUCT_DEVICE := corvette
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
